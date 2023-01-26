4d4433ffe36ffffa394a214164878aa7f87c868e
rachiderrami <errami.rachid@outlook.com>
2023-01-26 09:09:46 -0500

    book store front navbar and loging form

---
+++ script.js
@@ -0,0 +1,31 @@
+const searchbtn = document.querySelector("#search-btn");
+const searchForm = document.querySelector(".header-1 .search-form");
+
+window.onscroll = () => {
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+    searchForm.classList.remove("active");
+  }
+};
+
+window.onload = () => {
+  document.querySelector(".header .header-2").classList.remove("active");
+  window.scrollTo(0, 0);
+  searchForm.classList.remove("active");
+};
+
+searchbtn.addEventListener("click", () => {
+  searchForm.classList.toggle("active");
+});
+
+let loginForm = document.querySelector(".login-form-container");
+document.querySelector('#login-btn').onclick = () => {
+  loginForm.classList.toggle('active')
+}
+
+document.querySelector("#close-login-btn").onclick = () => {
+  loginForm.classList.remove("active");
+};
+
